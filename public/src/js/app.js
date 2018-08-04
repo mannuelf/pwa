b87e92bc85c58cde859f9df7031f26fdccb69b8d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-08-04 23:30:25 +0200

    fetch GET request

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -19,15 +19,31 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false
 })
 
+
+fetch('https://httpbin.org/get')
+  .then(function(response) {
+    console.log('[fetch]', response)
+    return response.json()
+})
+.then(function(data) {
+  console.log('[fetch data]', data)
+})
+.catch(function(err) {
+  console.log('[fetch error]', err)
+})
+
 var promise = new Promise(function(resolve, reject) {
   setTimeout(function() {
-    resolve('This is executed once the timer is done!')
+    // resolve('This is executed once the timer is done!')
+    reject({ code: 500, message: 'An ERROR occurred!' })
     // console.log('executed after 3 seconds');
   }, 3000)
 })
 
 promise.then(function(text) {
   return text
+}, function(err) {
+  console.log(err.code, err.message)
 }).then(function(newText) {
   console.log(newText)
 })
